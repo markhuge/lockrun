295be68be8758e50b120709445d0e7d4dfc092ab
Mark Wilkerson <mark@markwilkerson.me>
2018-01-16 03:49:04 -0800

    add helptext

--- cli.go
+++ cli.go
@@ -3,13 +3,27 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 )
 
 var (
-	lockfile  = flag.String("lockfile", "", "create a lockfile at <path>. (default /var/run/<command>")
+	lockfile  = flag.String("lockfile", "", "create a lockfile at <path>. (default /var/run/<command>)")
 	pidfile   = flag.String("pidfile", "", "explicitly create a pidfile at <path>. Without this option, lockfile will be used as a pidfile")
 	ignorepid = flag.Bool("ignore-pidfile", false, "ignore any pidfiles found for <command>")
 )
+
+var usage = `
+lockrun: execute a command with overrun protection
+
+Usage: lockrun [OPTION]... COMMAND [COMMAND ARGUMENTS]...
+
+`
+
+var footer = `
+Bug reports: https://github.com/markhuge/lockrun/issues
+License: MIT https://github.com/markhuge/lockrun/blob/master/LICENSE
+
+`
 
 // Opts contains the CLI options resolved against the defaults
 type Opts struct {
@@ -27,10 +41,22 @@ func (o *Opts) usePidfile() bool {
 
 // ParseCLI instantiates a new Opts object with options passed from CLI flags
 func ParseCLI() Opts {
+	// Help text
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, usage)
+		flag.PrintDefaults()
+		fmt.Fprintf(os.Stderr, footer)
+	}
+
 	flag.Parse()
 	args := flag.Args()
 
 	opts := new(Opts)
+
+	if len(args) < 1 {
+		flag.Usage()
+		os.Exit(2)
+	}
 
 	opts.Command = args[0]
 	opts.CommandArgs = args[1:]
